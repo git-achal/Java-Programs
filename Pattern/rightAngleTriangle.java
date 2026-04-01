a48f3f9228a8cbedfe0d30f1a98183523b774ddf
Achal Chaudhari <achalchaudhari25@gmail.com>
2026-04-01 12:45:40 +0530

    Add files via upload

---
+++ Pattern/rightAngleTriangle.java
@@ -0,0 +1,13 @@
+package Pattern;
+
+public class rightAngleTriangle {
+public static void main(String[] args) {
+	int star=5;
+	for(int i=1;i<=star;i++) {
+		for(int j=1;j<=i;j++) {
+			System.out.print("*");
+		}
+		System.out.println(	);
+	}
+}
+}
